037b10b2fc977d206c91819794203104b5d33754
leoxs22 <leandro.narosky@gmail.com>
2022-04-23 13:59:39 -0300

    Visual changes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Card, Container, FormControl, InputGroup, ListGroup, ListGroupItem, Table} from "react-bootstrap";
+import {Card, Col, Container, FormControl, InputGroup, ListGroup, Row, Table} from "react-bootstrap";
 import {useEffect, useState} from "react";
 import CardHeader from "react-bootstrap/CardHeader";
+import {blocksPerYear, colateralRatio, interests} from "./constants";
+import NumberFormat from 'react-number-format';
+import {FaTwitter, FaLinkedinIn, FaGithub} from "react-icons/fa";
+import numLogo from "./images/numlogo.png";
+
 const axios = require('axios').default;
 
-const blocksPerYear = 4656810 // From anchor repo
-
-const interests = [{
-    days: 90,
-    discount: 0.35
-},
-{
-    days: 180,
-    discount: 0.39
-},
-{
-    days: 270,
-    discount: 0.42
-},
-{
-    days: 360,
-    discount: 0.43
-}]
-
-const colateralRatio = 1.2;
+function formatNumber(num) {
+    return <NumberFormat value={num} displayType={'text'} thousandSeparator={"."} decimalSeparator={","}
+                         decimalScale={2} className={"numbers"}/>
+}
 
 function App() {
 
@@ -32,26 +21,29 @@ function App() {
     const [anchorAPY, setAnchorAPY] = useState(null);
     const [selectedDays, setSelectedDays] = useState(90);
     const [requestedAmount, setRequestedAmount] = useState(500000);
+    const [lastUpdated, setLastUpdated] = useState({anchor: null, num: null});
 
     const selectedInterest = interests.find(i => i.days === selectedDays);
-    const interest = requestedAmount * (selectedInterest.discount)/360*selectedDays;
+    const interest = requestedAmount * (selectedInterest.discount) / 360 * selectedDays;
     const amountToReceive = requestedAmount - interest;
 
     useEffect(() => {
         axios.get('https://api.pancakeswap.info/api/v2/tokens/0x91bc956F064d755dB2e4EfE839eF0131e0b07E28')
-        .then(res => {
-            console.log(res.data.data.price);
-            setNuArsPrice(parseFloat(res.data.data.price));
-        })
-        .catch(err => {
-            console.log(err);
-        })
+            .then(res => {
+                setNuArsPrice(parseFloat(res.data.data.price));
+                setLastUpdated((prev) => ({...prev, num: new Date()}));
+            })
+            .catch(err => {
+                console.log(err);
+            })
 
         // get APY anchor earn
         axios.get('https://api.anchorprotocol.com/api/v2/deposit-rate')
-        .then(res => {
-            console.log(res.data[0].deposit_rate);
-            setAnchorAPY(parseFloat(res.data[0].deposit_rate) * blocksPerYear);
+            .then(res => {
+                setAnchorAPY(parseFloat(res.data[0].deposit_rate) * blocksPerYear);
+                setLastUpdated((prev) => ({...prev, anchor: new Date()}));
+            }).catch(err => {
+            console.log(err);
         })
     }, [])
 
@@ -66,69 +58,148 @@ function App() {
 
     const requiredDolarIncrease = dolarPriceNeededToCoverInterest * nuArsPrice - 1;
     return (
-    <div className="App">
-        <Container>
-            <Card>
-                <CardHeader>
-                    <h1>Intereses (Actualizado 22/04/2022)</h1>
-                </CardHeader>
-             <Table>
-                <thead>
-                    <tr>
-                        <th>Días</th>
-                        <th>Tasa de descuento</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        interests.map(interest => {
-                            return <tr>
-                                <td>{interest.days}</td>
-                                <td>{interest.discount * 100}%</td>
-                            </tr>
-                        })
-                    }
-                </tbody>
-             </Table>
-            </Card>
-            <Card>
-                <ListGroup>
-                    <ListGroup.Item>Precio dólar (pancake): ${(1/nuArsPrice).toFixed(2)}</ListGroup.Item>
-                    <ListGroup.Item>Anchor APY: {(anchorAPY*100).toFixed(2)}%</ListGroup.Item>
-                    <ListGroup.Item>Ratio colateral {colateralRatio*100}%</ListGroup.Item>
-                </ListGroup>
-            </Card>
-            <Card>
-                <select className="form-select" aria-label="Default select" onChange={(e) => setSelectedDays(Number(e.target.value))}>
-                    {interests.map(interest =>
-                        <option value={interest.days} selected={interest.days === selectedDays}>{interest.days} días</option>
-                    )}
-                </select>
-                <InputGroup className="mb-3">
-                    <InputGroup.Text id="requested_amount">Monto solicitado $</InputGroup.Text>
-                    <FormControl
-                        aria-label="requested_amount"
-                        aria-describedby="requested_amount"
-                        value={requestedAmount}
-                        onChange={(e) => setRequestedAmount(e.target.value)}
-                    />
-                </InputGroup>
-                <ListGroup>
-                    <ListGroup.Item><b>Total a recibir:</b> ${amountToReceive.toFixed(2)}</ListGroup.Item>
-                    <ListGroup.Item><b>Total intereses:</b> ${interest.toFixed(2)}</ListGroup.Item>
-                    <ListGroup.Item><b>Colateral necesario</b> U$D {colateralNeeded.toFixed(2)} </ListGroup.Item>
-                    <ListGroup.Item><b>Ganancia esperada en Anchor</b> U$D {expectedAnchorEarnings.toFixed(2)}</ListGroup.Item>
-                    <ListGroup.Item><b>Ganancia esperada en Anchor (sin colateral extra)</b> U$D {expectedAnchorEarningsWithoutColateral.toFixed(2)}</ListGroup.Item>
-                    <ListGroup.Item><b>Precio del dolar necesario para cubrir interés</b> $ {dolarPriceNeededToCoverInterest.toFixed(2)}</ListGroup.Item>
-                    <ListGroup.Item><b>Aumento del dólar necesario</b> {(requiredDolarIncrease*100).toFixed(2)}% en {selectedDays} días / {((((requiredDolarIncrease+1)**(30/90))-1)*100).toFixed(2)}% mensual / {((((requiredDolarIncrease+1)**(365/90)-1)*100)).toFixed(2)}% anual</ListGroup.Item>
-                </ListGroup>
-            </Card>
-        </Container>
+        <div className={"outside"}>
+            <Container className={"app"}>
+                <div>
+                    <Card>
+                        <CardHeader>
+                                <Row>
+                                    <Col>
+                                        <img src={numLogo} className={"logo"}/>
+                                    </Col>
+                                    <Col className={"col-auto"}>
+                                        <h2 className={"m-0"}>Calculadora Num Finance</h2>
+                                        <h5 className={"p-0 text-muted"}>(no oficial)</h5>
+                                    </Col>
+                                </Row>
+                        </CardHeader>
 
-
-
-    </div>
-  );
+                    </Card>
+                    <Card>
+                        <ListGroup>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Precio dólar</Col>
+                                    <Col className="col-auto">${formatNumber(1 / nuArsPrice)} </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Anchor APY</Col>
+                                    <Col className="col-auto">{formatNumber(anchorAPY * 100)}% </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Ratio colateral</Col>
+                                    <Col className="col-auto">{formatNumber(colateralRatio * 100)}%</Col>
+                                </Row>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Card>
+                    <Card className="p-3">
+                        <select className="form-select mb-3" aria-label="Default select"
+                                onChange={(e) => setSelectedDays(Number(e.target.value))}>
+                            {interests.map(interest =>
+                                <option value={interest.days}
+                                        selected={interest.days === selectedDays}>{interest.days} días - Tasa de
+                                    descuento {interest.discount * 100}%</option>
+                            )}
+                        </select>
+                        <InputGroup>
+                            <InputGroup.Text id="requested_amount">Monto solicitado $</InputGroup.Text>
+                            <FormControl
+                                aria-label="requested_amount"
+                                aria-describedby="requested_amount"
+                                value={requestedAmount}
+                                onChange={(e) => setRequestedAmount(e.target.value)}
+                            />
+                        </InputGroup>
+                    </Card>
+                    <Card>
+                        <ListGroup>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Total a recibir</Col>
+                                    <Col className="col-auto">${formatNumber(amountToReceive)}</Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Total intereses</Col>
+                                    <Col className="col-auto">${formatNumber(interest)}</Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Colateral necesario</Col>
+                                    <Col className="col-auto">U$D {formatNumber(colateralNeeded)}</Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Ganancia en Anchor</Col>
+                                    <Col className="col-auto">U$D {formatNumber(expectedAnchorEarnings)}</Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Ganancia en Anchor <span
+                                        className={"text-muted"}>sin colateral extra</span></Col>
+                                    <Col
+                                        className="col-auto">U$D {formatNumber(expectedAnchorEarningsWithoutColateral)}</Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Precio del dólar al que se paga solo</Col>
+                                    <Col className="col-auto">$ {formatNumber(dolarPriceNeededToCoverInterest)}</Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Aumento del dólar necesario</Col>
+                                    <Col className="col-auto">
+                                        <p className={"mb-0"}>
+                                            <div className={"float-right"}>
+                                                <span className={"text-muted"}>mensual </span>
+                                                {formatNumber((((requiredDolarIncrease + 1) ** (30 / selectedDays)) - 1) * 100)}%
+                                            </div>
+                                        </p>
+                                        <p className={"mb-0"}>
+                                            <div className={" float-right"}>
+                                                <span className={"text-muted"}> en {selectedDays} días </span>
+                                                {formatNumber(requiredDolarIncrease * 100)}%
+                                            </div>
+                                        </p>
+                                        <p className={"mb-0"}>
+                                            <div className={"float-right"}>
+                                                <span className={"text-muted"}>anual </span>
+                                                {formatNumber((((requiredDolarIncrease + 1) ** (365 / selectedDays) - 1) * 100))}%
+                                            </div>
+                                        </p>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Card>
+                    <Card>
+                        <div className={"text-center p-1"}>
+                            Hecho con ♥ por leoxs. Seguime!
+                            <a href={"http://twitter.narosky.com"} target="_blank" className={"px-1"}><FaTwitter/></a>
+                            <a href={"http://linkedin.narosky.com"} target="_blank"
+                               className={"px-1"}><FaLinkedinIn/></a>
+                            <a href={"http://github.narosky.com"} target="_blank" className={"px-1"}><FaGithub/></a>
+                        </div>
+                    </Card>
+                    <Card className={"p-2"}>
+                        <div className={"text-muted"}>Intereses de num actualizados al 22/04/2022</div>
+                        <div className={"text-muted"}>Puede haber errores en los cálculos, do your own research</div>
+                    </Card>
+                </div>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const blocksPerYear = 4656810 // From anchor repo
+
+export const interests = [
+    {
+        days: 90,
+        discount: 0.35
+    },
+    {
+        days: 180,
+        discount: 0.39
+    },
+    {
+        days: 270,
+        discount: 0.42
+    },
+    {
+        days: 360,
+        discount: 0.43
+    }]
+
+export const colateralRatio = 1.2;
